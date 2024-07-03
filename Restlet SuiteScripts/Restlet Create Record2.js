c2dd6171a1f502273bbf027737a29fde7fabe7e6
gopikrish1202 <68181485+gopikrish1202@users.noreply.github.com>
2024-07-03 15:13:10 +0530

    record created successfully

--- Restlet SuiteScripts/Restlet Create Record2.js
+++ Restlet SuiteScripts/Restlet Create Record2.js
@@ -3,35 +3,51 @@
  * @NScriptType Restlet
  */
 define(['N/record', 'N/log'], function(record, log) {
-    /**
-    * @param {Object} requestBody
-    * @returns {string | Object}
-    * @since 2015.2
-    */
-   function post(requestBody){
-    log.debug("Post triggered",JSON.stringify(requestBody))
-   
+  function post(requestBody) {
+      log.debug("Post triggered", JSON.stringify(requestBody));
 
-  var objRecord=record.create({
-    type: requestBody.recordType
-  })
-  var fieldId=requestBody.fieldId;
-  var values=requestBody.values;
+      var objRecord = record.create({
+          type: requestBody.recordType
+      });
 
-  for(var i=0;i<values.length;i++){
-    objRecord.setValue({
-        fieldId: fieldId[i],
-        value: values[i]
-      })
+      // Set main line fields
+      var mainLineFields = requestBody.mainLineFields;
+      for (var i = 0; i < mainLineFields.length; i++) {
+          objRecord.setValue({
+              fieldId: mainLineFields[i].fieldId,
+              value: mainLineFields[i].value
+          });
+      }
+
+      // Handle sublist data
+      var sublistData = requestBody.sublistData;
+      for (var j = 0; j < sublistData.length; j++) {
+          var sublist = sublistData[j];
+          var sublistId = sublist.sublistId;
+          var sublistItems = sublist.sublistItems;
+
+          // Add sublist items
+          for (var k = 0; k < sublistItems.length; k++) {
+              var sublistItem = sublistItems[k];
+              for (var fieldId in sublistItem) {
+                  objRecord.setSublistValue({
+                      sublistId: sublistId,
+                      fieldId: fieldId,
+                      line: k,
+                      value: sublistItem[fieldId]
+                  });
+              }
+          }
+      }
+
+      var savedId = objRecord.save();
+      return {
+          success: true,
+          recordId: savedId
+      };
   }
-  var savedId = objRecord.save();
-  return log.debug(JSON.stringify("Record created successfully with id",savedId))
-}
 
- 
-   return{
-    post: post
-    
-   }
-
-})
+  return {
+      post: post
+  };
+});
